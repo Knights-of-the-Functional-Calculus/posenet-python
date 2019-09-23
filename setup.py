088525c0a401e707ef218b9b06645902ba78146f
Jack Frost <j4qfrost@gmail.com>
2019-09-22 23:58:53 -0700

    expose process function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='posenet-tf',
-    version='0.0.2',
+    version='0.0.3',
     packages=['posenet','posenet.converter'],
     package_data={'': ['converter/config.yaml']},
 	include_package_data=True,
