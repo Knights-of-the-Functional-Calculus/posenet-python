ef292d847ec9ff3554146a4ed8f5db2635dbfbe9
Jack Frost <j4qfrost@gmail.com>
2019-09-22 23:49:05 -0700

    uptick

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='posenet-tf',
-    version='0.1',
+    version='0.0.2',
     packages=['posenet','posenet.converter'],
     package_data={'': ['converter/config.yaml']},
 	include_package_data=True,
